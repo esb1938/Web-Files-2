628ba6a2c1bd7e8244155af254f880b06b9a3ed3
esb1938 <89860582+esb1938@users.noreply.github.com>
2021-12-08 16:10:21 -0700

    Add files via upload

---
+++ week_12/portfolio.js
@@ -0,0 +1,24 @@
+document.getElementById("mybutton1").addEventListener("click", visitPage);
+  function visitPage() {
+     window.location.assign("https://esb1938.github.io/Web-Files-2/second%20week/index.html");
+  }
+document.getElementById("mybutton2").addEventListener("click", visitPage2);
+  function visitPage2() {
+         window.location.assign("https://esb1938.github.io/Web-Files-2/index.html");
+  }
+document.getElementById("mybutton3").addEventListener("click", visitPage3);
+  function visitPage3() {
+        window.location.assign("https://esb1938.github.io/Web-Files-2/seventh_week/index.html");
+  }
+document.getElementById("mybutton4").addEventListener("click", visitPage4);
+  function visitPage4() {
+        window.location.assign("https://esb1938.github.io/Web-Files-2/eigth_week/index.html");
+  }
+document.getElementById("mybutton5").addEventListener("click", visitPage5);
+    function visitPage5() {
+        window.location.assign("https://spillonthehill.com/");
+  }
+document.getElementById("mybutton6").addEventListener("click", visitPage6);
+    function visitPage6() {
+        window.location.assign("https://www.linkedin.com/in/esther-barnett-535a2a180/");
+    }
